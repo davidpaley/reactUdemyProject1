746f20b4f058ab7f3bf9823d2817d34b3dcffe45
davidpaley <paley@strands.com>
2017-11-10 12:25:01 -0300

    Full Indecision web project with the css customized with SCSS

--- src/components/AddOption.js
+++ src/components/AddOption.js
@@ -6,7 +6,6 @@ export default class AddOption extends React.Component {
   };
   handleAddOption = (e) => {
     e.preventDefault();
-    console.log("adding a new option");
     const option = e.target.elements.option.value.trim();
     const error = this.props.handleAddOption(option);
 
@@ -19,10 +18,10 @@ export default class AddOption extends React.Component {
   render() {
     return (
       <div>
-        {this.state.error && <p>{this.state.error}</p>}
-        <form onSubmit={this.handleAddOption}>
-          <input type="text" name="option" />
-          <button>Add Option</button>
+        {this.state.error && <p className="add-option-error">{this.state.error}</p>}
+        <form className="add-option" onSubmit={this.handleAddOption}>
+          <input className="add-option__input" type="text" name="option" />
+          <button className="button">Add Option</button>
         </form>
       </div>
     );
